f7fd4fd451a03616dfec8bac24076b465fe9d8ff
Krzysztof Telatycki <krzysztof.telatycki@gmail.com>
2018-01-03 00:29:10 +0100

    DATA

---
+++ konwerter.php
@@ -0,0 +1,24 @@
+<?php
+
+class Konwerter
+ {
+
+
+
+public $data = [ 
+
+		0=>'', 1 =>'JEDEN' , 2=>'DWA' , 3 =>'TRZY' ,4=>'CZTERY' , 5 => 'PIĘŃĆ' , 6=> 'SZEŚĆ' , 7 => 'SIEDEM' , 8=>'OSIEM' , 9=>'DZIEWIEŃĆ',
+
+		10=>'DZIESIĘŃĆ', 11=>'JEDYNAŚCIE' , 12=> 'DWANAŚCIE' , 13 =>'TRZYNAŚCIE' , 14 =>'CZTERNAŚCIE', 15=>'PIĘTNAŚCIE' ,
+		16=>'SZESNAŚCIE', 17=>'SIEDEMNASCIE', 18=>'OSIEMNAŚCIE' , 19=>'DZIEWIĘTNAŚCIE' , 20=>'DWADZIEŚCIA' , 
+		30=>'TRZYDZIEŚCI', 40=> 'CZTERDZIEŚCI' , 50=>'PIĘĆIESIĄT', 60=>'SZEŚĆDZIESIĄT',70=>'SIEDEMDZIESIĄT',80=>'OSIEMDZIESIĄT',
+		90=>'DZIEWIĘĆDZIESIĄT', 100=>'STO' , 200=>'DWIEŚCIE', 300=>'TRZYSTA', 400=>'CZTERYSTA',500=>'PIĘĆSET',600=>'SZEŚĆSET',
+		700=>'SIEDEMSET',800=>'OSIEMSET' , 900=>'DZIEWIĘĆSET' , 1000=>'TYSIĄC', 
+
+
+		];
+	
+
+
+
+}
